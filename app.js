8600380add2bc46c1bf18ec7c575da687c31bf21
shaku1 <ag051823@gmail.com>
2023-11-01 19:20:54 -0300

    Actualizando index.html app.js y sobre-nosotros.html

--- app.js
+++ app.js
@@ -11,3 +11,18 @@ app.listen(PORT,() => console.log(`Server listening on port ${PORT}`))
 app.get("/home", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 })
+app.get("/sobre-nosotros", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/sobre-nosotros.html"))
+})
+app.get("/iniciar-sesion", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+app.get("/carrito-de-compras", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
+})
+app.get("/registrarme", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+app.get("/detalles-de-producto", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
+})
